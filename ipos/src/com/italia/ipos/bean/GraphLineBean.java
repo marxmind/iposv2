ea9741f737b23b72121f6571e9b90e36c678441e
Mark <Mark@Mark-Acer.domain.name>
2022-06-22 10:12:18 +0800

    initial upload

---
+++ ipos/src/com/italia/ipos/bean/GraphLineBean.java
@@ -0,0 +1,183 @@
+package com.italia.ipos.bean;
+
+import javax.annotation.PostConstruct;
+import java.io.Serializable;
+import java.util.Map;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.LineChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LegendPlacement;
+import com.italia.ipos.controller.DeliveryItemTrans;
+import com.italia.ipos.controller.Product;
+
+@Named("graphlineBean")
+@ViewScoped
+public class GraphLineBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5424647856813632L;
+
+	private LineChartModel lineModel1;
+    private LineChartModel lineModel2;
+     
+    
+    @ManagedProperty("#{delprodmonBean}")
+	private DeliveryProductMonitoringBean monitoring;
+	public void setMonitoring(DeliveryProductMonitoringBean monitoring){
+		this.monitoring = monitoring;
+	}
+	public DeliveryProductMonitoringBean getMonitoring(){
+		return monitoring;
+	}
+    
+    @PostConstruct
+    public void init() {
+        createLineModels();
+    }
+ 
+    public LineChartModel getLineModel1() {
+        return lineModel1;
+    }
+ 
+    public LineChartModel getLineModel2() {
+        return lineModel2;
+    }
+     
+    private void createLineModels() {
+    	Axis yAxis = null;
+    	
+        
+         
+    	String title = "Product Statistic Report";
+    	String rangeTitle = "Day";
+    	if(1==getMonitoring().getRangeId()){//per day
+    		title = "Per Day " + title;
+    		rangeTitle = "Day";
+    	}else if(2==getMonitoring().getRangeId()){//per month
+    		title = "Per Month " + title;
+    		rangeTitle = "Month";
+    	}else if(3==getMonitoring().getRangeId()){//per year
+    		title = "Per Year " + title;
+    		rangeTitle = "Year";
+    	}
+    	
+        lineModel2 = initCategoryModel();
+        lineModel2.setLegendPlacement(LegendPlacement.OUTSIDE);
+        lineModel2.setShowPointLabels(true);
+        lineModel2.setZoom(true);
+        lineModel2.setTitle(title);
+        lineModel2.setLegendPosition("e");
+        lineModel2.setShowPointLabels(true);
+        lineModel2.getAxes().put(AxisType.X, new CategoryAxis(rangeTitle));
+        yAxis = lineModel2.getAxis(AxisType.Y);
+        yAxis.setLabel("Quantity");
+        yAxis.setMin(0);
+        if(getMonitoring().getRangeId()==1){
+        	yAxis.setMax(500);
+        }else if(getMonitoring().getRangeId()==2){
+        	yAxis.setMax(2000);
+        }else if(getMonitoring().getRangeId()==3){
+        	yAxis.setMax(30000);
+        }
+        Axis xAxis = lineModel2.getAxis(AxisType.X); 
+        //xAxis.setTickFormat("Php%'d");
+        xAxis.setTickFormat("%'d");
+        
+        //yAxis.setTickFormat("Php%'d");
+        yAxis.setTickFormat("%'d");
+        
+    }
+     
+    private LineChartModel initLinearModel() {
+        LineChartModel model = new LineChartModel();
+        
+       
+        
+         
+        return model;
+    }
+     
+    private LineChartModel initCategoryModel() {
+        
+    	LineChartModel model = new LineChartModel();
+    	
+        if(monitoring.getTransactionData()!=null && monitoring.getTransactionData().size()>0){
+        	Map<String, Map<Long, DeliveryItemTrans>> data = getMonitoring().getTransactionData();
+        	
+        	for(Product prod : getMonitoring().getProductData().values()){
+        		
+        		ChartSeries series = new ChartSeries();
+        		series.setLabel(prod.getProductProperties().getProductname());
+        		
+            	for(String date : data.keySet()){
+            		DeliveryItemTrans tran = null;
+            		tran = data.get(date).get(prod.getProdid());
+            		
+            		if(getMonitoring().getRangeId()==1){
+            		
+            			if(tran!=null){
+            				series.set(date, tran.getQuantity());
+            			}else{
+            				series.set(date, 0);
+            			}
+            		
+            		}else if(getMonitoring().getRangeId()==2){
+            			if("01".equalsIgnoreCase(date)){
+            				date = "January";
+            			}else if("02".equalsIgnoreCase(date)){
+            				date = "February";
+            			}else if("03".equalsIgnoreCase(date)){
+            				date = "March";
+            			}else if("04".equalsIgnoreCase(date)){
+            				date = "April";
+            			}else if("05".equalsIgnoreCase(date)){
+            				date = "May";
+            			}else if("06".equalsIgnoreCase(date)){
+            				date = "June";
+            			}else if("07".equalsIgnoreCase(date)){
+            				date = "July";
+            			}else if("08".equalsIgnoreCase(date)){
+            				date = "August";
+            			}else if("09".equalsIgnoreCase(date)){
+            				date = "September";
+            			}else if("10".equalsIgnoreCase(date)){
+            				date = "October";
+            			}else if("11".equalsIgnoreCase(date)){
+            				date = "November";
+            			}else if("12".equalsIgnoreCase(date)){
+            				date = "December";
+            			}
+            			
+            			
+            			if(tran!=null){
+            				series.set(date, tran.getQuantity());
+            			}else{
+            				series.set(date, 0);
+            			}
+            		}else{
+            			if(tran!=null){
+            				series.set(date, tran.getQuantity());
+            			}else{
+            				series.set(date, 0);
+            			}
+            		}
+            		
+            	}
+        		model.addSeries(series);
+        	}
+        
+        }
+        
+         
+        return model;
+    }
+ 
+}
---
+++ ipos/src/com/italia/ipos/utils/Fields.java
@@ -0,0 +1,36 @@
+package com.italia.ipos.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * 
+ * @author mark italia
+ * @since 09/28/2016
+ *
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Builder
+@ToString
+public class Fields {
+
+	private String f1;
+	private String f2;
+	private String f3;
+	private String f4;
+	private String f5;
+	private String f6;
+	private String f7;
+	private String f8;
+	private String f9;
+	private String f10;
+	private String f11;
+	private String f12;
+}
